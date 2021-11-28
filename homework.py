362602b450b95416b2eec12c1b0f88a72612df05
Makar <diakonov.makar@gmail.com>
2021-11-28 23:16:40 +0100

    The 1st commit

--- homework.py
+++ homework.py
@@ -1,82 +1,60 @@
-...
+import os
+import time
+from datetime import datetime
+import logging
+
+import telegram
+from dotenv import load_dotenv
 
 load_dotenv()
 
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
-
-RETRY_TIME = 600
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-
-
-HOMEWORK_STATUSES = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.'
-}
+RETRY_TIME = 60
+HOURS_5 = 3600 * 5
 
 
 def send_message(bot, message):
-    ...
+    bot.send_message(TELEGRAM_CHAT_ID, message)
+    logging.info(f'Бот отправил сообщение {message}')
 
 
-def get_api_answer(current_timestamp):
-    timestamp = current_timestamp or int(time.time())
-    params = {'from_date': timestamp}
-
-    ...
-
-
-def check_response(response):
-
-    ...
-
-
-def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
-
-    ...
-
-    verdict = ...
-
-    ...
-
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
-
+"""
+def check_response(bot):
+    updates = bot.get_updates()
+    text_of_last = updates[0]['message']['text']
+    if text_of_last in ['ОК', 'Ок', 'ок', 'OK', 'Ok', 'ok', 'Выпила', 'выпила', 'Спасибо', 'спасибо']:
+        return True
+    else:
+        return False
+"""
 
 def check_tokens():
-    ...
+    error_msg = 'Отсутствует обязательная переменная окружения:'
+    if TELEGRAM_TOKEN is None:
+        logging.critical(f'{error_msg} "TELEGRAM_TOKEN".')
+        return False
+    if TELEGRAM_CHAT_ID is None:
+        logging.critical(f'{error_msg} "TELEGRAM_CHAT_ID".')
+        return False
+    return True
 
 
 def main():
     """Основная логика работы бота."""
-
-    ...
-
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
-
-    ...
-
+    utc_time = datetime.utcnow()
     while True:
         try:
-            response = ...
-
-            ...
-
-            current_timestamp = ...
+            if utc_time.hour == 19 and utc_time.minute == 30:
+                send_message(bot, 'Леночка, выпей таблетку \n:*')
             time.sleep(RETRY_TIME)
-
+            utc_time = datetime.utcnow()
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
+            logging.error(message)
             time.sleep(RETRY_TIME)
-        else:
-            ...
 
 
 if __name__ == '__main__':
