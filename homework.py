1d3edd97dd5bfd836b1fe0a7ddf7928e4e72d672
Makar <diakonov.makar@gmail.com>
2021-12-22 13:20:16 +0100

    Debug

--- homework.py
+++ homework.py
@@ -10,23 +10,25 @@ load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-OWNER_CHAT_ID = os.getenv('OWNER_CHAT_ID')
 
 RETRY_TIME = 60
 HOURS_5 = 3600 * 5
 
 
-def send_message(bot, chat_id, message):
-    bot.send_message(chat_id, message)
+def send_message(bot, message):
+    bot.send_message(TELEGRAM_CHAT_ID, message)
     logging.info(f'Бот отправил сообщение {message}')
 
 
 """
-def check_response(bot, response):
-    
-    last_author_id = updates[0]['message']
+def check_response(bot):
+    updates = bot.get_updates()
+    text_of_last = updates[0]['message']['text']
+    if text_of_last in ['ОК', 'Ок', 'ок', 'OK', 'Ok', 'ok', 'Выпила', 'выпила', 'Спасибо', 'спасибо']:
+        return True
+    else:
+        return False
 """
-
 
 def check_tokens():
     error_msg = 'Отсутствует обязательная переменная окружения:'
@@ -46,16 +48,8 @@ def main():
     while True:
         try:
             if utc_time.hour == 19 and utc_time.minute == 30:
-                message = 'Леночка, выпей таблетку \n:*'
-                send_message(bot, TELEGRAM_CHAT_ID, message)
+                send_message(bot, 'Леночка, выпей таблетку \n:*')
             time.sleep(RETRY_TIME)
-
-            updates = bot.get_updates()
-            text_of_last = updates[0]['message']['text']
-            if text_of_last != message:
-                send_message(bot, OWNER_CHAT_ID, text_of_last)
-                message = text_of_last
-
             utc_time = datetime.utcnow()
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
